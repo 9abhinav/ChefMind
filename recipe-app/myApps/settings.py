3c711f5b44cc9582515ef7b1e230308187b80af8
9abhinav <abhinavdewangan64@gmail.com>
2024-12-08 10:02:49 +0530

    Update settings.py

--- recipe-app/myApps/settings.py
+++ recipe-app/myApps/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# The directory where Django will collect static files for production
+STATIC_ROOT = '/home/abhinavdewa65/ChefMind/recipe-app/static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
